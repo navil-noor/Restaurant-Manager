88d059fd2e423b99ec1452e7f5818b500064284f
Navil Noor <92982456+navil-noor@users.noreply.github.com>
2023-11-25 19:38:04 +0100

    added code for unit test

--- RestaurantManager/FoodApp/tests.py
+++ RestaurantManager/FoodApp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Restaurant, Recipe, Ingredient
 
-# Create your tests here.
+
+class RestaurantTestCase(TestCase):
+    def setUp(self):
+        # Create test data
+        ingredient = Ingredient.objects.create(name='Test Ingredient')
+        recipe = Recipe.objects.create(name='Test Recipe')
+        recipe.ingredients.add(ingredient)
+        restaurant = Restaurant.objects.create(name='Test Restaurant')
+        restaurant.recipes.add(recipe)
+
+    def test_restaurant_has_recipe(self):
+        restaurant = Restaurant.objects.get(name='Test Restaurant')
+        self.assertEqual(restaurant.recipes.count(), 1)
