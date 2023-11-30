6226927b2a4c94fd4497c0e0becc7ad3abce900e
Navil Noor <92982456+navil-noor@users.noreply.github.com>
2023-11-30 17:41:15 +0100

    added filter option

--- RestaurantManager/FoodApp/admin.py
+++ RestaurantManager/FoodApp/admin.py
@@ -5,6 +5,7 @@ from .models import Ingredient, Recipe, Restaurant
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'display_ingredients', 'display_restaurants')
+    list_filter = ('ingredients', 'restaurants')
 
     def display_ingredients(self, obj):
         return ", ".join([ingredient.name for ingredient in obj.ingredients.all()])
@@ -20,6 +21,7 @@ class RecipeAdmin(admin.ModelAdmin):
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('name', 'display_recipes')
+    list_filter = ('recipes',)
 
     def display_recipes(self, obj):
         return ", ".join([recipe.name for recipe in obj.recipes.all()])
@@ -30,6 +32,7 @@ class RestaurantAdmin(admin.ModelAdmin):
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'display_recipes', 'display_restaurants')
+    list_filter = ('recipes', 'recipes__restaurants')  # Use the actual fields with __ notation
 
     def display_recipes(self, obj):
         return ", ".join([recipe.name for recipe in obj.recipes.all()])
@@ -37,6 +40,6 @@ class IngredientAdmin(admin.ModelAdmin):
     display_recipes.short_description = 'Recipes'
 
     def display_restaurants(self, obj):
-        return ", ".join([restaurant.name for recipe in obj.recipes.all() for restaurant in recipe.restaurants.all()])
+        return ", ".join([restaurant.name for restaurant in obj.recipes.all() for restaurant in restaurant.restaurants.all()])
 
     display_restaurants.short_description = 'Restaurants'
